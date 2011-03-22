a8069b54045eca42f875b3bb52622e742548fc6d
Ian Johnson <enjalot@gmail.com>
2011-03-21 23:03:29 -0400

    slight update to match new render settings in RTPS

--- blender/source/gameengine/Converter/BL_ModifierDeformer.cpp
+++ blender/source/gameengine/Converter/BL_ModifierDeformer.cpp
@@ -415,7 +415,7 @@ bool BL_ModifierDeformer::Apply(RAS_IPolyMaterial *mat)
                         rtps::RTPSettings settings(sys, rtmd->num, rtmd->dt, grid, rtmd->collision);
 						//GE should automate with python
 						settings.setRadiusScale(rtmd->render_radius_scale);
-						settings.setRenderType(rtmd->render_type);
+						settings.setRenderType((rtps::RTPSettings::RenderType)rtmd->render_type);
 						settings.setBlurScale(rtmd->render_blur_scale);
 						settings.setUseGLSL(rtmd->glsl);
 						settings.setUseAlphaBlending(rtmd->blending);
--- environment/setupenv_linux_enj.sh
+++ environment/setupenv_linux_enj.sh
@@ -1,2 +1,3 @@
-export RTPS_DIR="/panfs/panasas1/users/idj03/research/rtps/rtps"
+export RTPS_DIR="/panfs/panasas1/users/idj03/research/master/rtps"
+#export RTPS_DIR="/panfs/panasas1/users/idj03/research/rtps/rtps"
 #export RTPS_DIR="/panfs/panasas1/users/idj03/research/rtps_ge/rtps"
